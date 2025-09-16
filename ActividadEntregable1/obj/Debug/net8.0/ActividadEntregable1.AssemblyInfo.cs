aaad051d88e529d9afbdf20f93daf45eed7a8909
Marcio Bulla <113257@tecnicatura.frc.utn.edu.ar>
2025-09-15 22:40:27 -0300

    se elimino crud de articulos

--- ActividadEntregable1/obj/Debug/net8.0/ActividadEntregable1.AssemblyInfo.cs
+++ ActividadEntregable1/obj/Debug/net8.0/ActividadEntregable1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActividadEntregable1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1dc8a415d3658877acfecccb0b5197fccc7c492")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActividadEntregable1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActividadEntregable1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
